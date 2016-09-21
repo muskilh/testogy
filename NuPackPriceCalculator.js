2dbaa9917965ab44b3d4bb4748cf5ba493d019bf
Irfan Shaikh <irfans@drawsplash.com>
2016-09-21 14:34:12 -0400

    Splitting price calc function further

--- NuPackPriceCalculator.js
+++ NuPackPriceCalculator.js
@@ -13,6 +13,24 @@ const BASE_MARKUP = 1.05;
 function priceCalc(basePrice, people, category) {
     return basePrice * BASE_MARKUP;
 }
+function priceCalcCore(basePrice, people, category) {
+    var out = Math.round(basePrice * 100) / 100 * BASE_MARKUP;
+    var markups = 1 + (people * 0.012);
+    
+    for (c of category) {
+        if (c == "food"){
+            markups += 0.13;
+        }
+        if (c == "pharmaceuticals"){
+            markups += 0.075;
+        }
+        if (c == "electronics"){
+            markups += 0.02;
+        }
+    }
+    
+    return (Math.round( (out * markups) *Math.pow(10,2))/Math.pow(10,2)).toFixed(2);
+}
 
 //Maps words into markup categories
 function wordsToMarkupCategories(words) {
