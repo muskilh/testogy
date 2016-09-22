6fc9aefdd093c9f6d07c62cf628c4a546890b1ec
Irfan Shaikh <irfans@drawsplash.com>
2016-09-21 20:27:27 -0400

    Implementing price calculator and fixing typos in tests

--- NuPackPriceCalculator.js
+++ NuPackPriceCalculator.js
@@ -3,21 +3,69 @@ const BASE_MARKUP = 1.05;
 
 
 /**
- * Adds markup to base price
+ * Adds markup to base price based on the number of people and mark up categories involved
  * @function priceCalc
  * @param {String} basePrice - base price in usd/cad currency
  * @param {String} people - number of people needed
- * @param {String} category - the markup category
+ * @param {String} categoryPhrase - the markup category/categories
  * @return {String} The final price quote
  */
-function priceCalc(basePrice, people, category) {
-    return basePrice * BASE_MARKUP;
+function priceCalc(basePrice, people, categoryPhrase) {
+    var out = 0;
+    
+    //First check the args and transform them into useable values; (aka double, int, str)
+    if (typeof basePrice === 'undefined' || basePrice == null)  throw new Error("Argument Missing");
+    if (typeof people === 'undefined' || people == null)  throw new Error("Argument Missing");
+    if (typeof categoryPhrase === 'undefined' || categoryPhrase == null) throw new Error("Argument Missing");
+    
+    var basePriceTransformed = basePrice;
+    var peopleTransformed = people;
+    var categoryPhraseTransformed = categoryPhrase;
+    
+    
+    //Convert the basePrice into a useable number
+    if (typeof basePriceTransformed === 'string'){ //If it's a str, we strip out any thing other than numbers and decimals
+        basePriceTransformed = Number(basePriceTransformed.replace(/[^\-0-9\.]+/g,""));
+        
+        //If it's not a number or negative, it's no good
+        if (isNaN(basePriceTransformed) || (basePriceTransformed < 0)){ 
+            throw new Error("Price Badly Formatted");
+        }
+    } else if (typeof basePriceTransformed !== 'number'){ //If anything other than str or number, it's no good
+        console.log(basePriceTransformed);
+        throw new Error("Price Badly Formatted");
+    }
+    
+    
+    //Convert people into a useable number
+    var regexFirstWord = /(?:^|\s)\w(.*?)(?:\s|$)/g;
+    var match = regexFirstWord.exec(peopleTransformed);
+    if (!match || match == null || match.length < 1){
+        throw new Error("People Badly Formatted");
+    }
+    peopleTransformed =  Number(match[0]);
+    if (isNaN(peopleTransformed) || (peopleTransformed !== parseInt(peopleTransformed, 10))){
+        throw new Error("People Badly Formatted");
+    }
+    
+    
+    //Convert Category Phrase into a list of official categories
+    if (typeof categoryPhrase !== 'string'){
+        throw new Error("Category Badly Formatted");
+    }
+    categoryPhraseTransformed = wordsToMarkupCategories(categoryPhraseTransformed);
+    
+    //Run the actual calculations
+    out = priceCalcCore(basePriceTransformed, peopleTransformed, categoryPhraseTransformed);
+    
+    //Format to proper string and return
+    return "$" + out.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
 }
 function priceCalcCore(basePrice, people, category) {
     var out = Math.round(basePrice * 100) / 100 * BASE_MARKUP;
     var markups = 1 + (people * 0.012);
     
-    for (c of category) {
+    for (var c of category) {
         if (c == "food"){
             markups += 0.13;
         }
@@ -29,18 +77,45 @@ function priceCalcCore(basePrice, people, category) {
         }
     }
     
-    return (Math.round( (out * markups) *Math.pow(10,2))/Math.pow(10,2)).toFixed(2);
+    //Apply markup
+    out = out * markups;
+    
+    //Round and force 2 decimal points
+    out = (Math.round( (out) *Math.pow(10,2))/Math.pow(10,2));
+    return out;
 }
 
 //Maps words into markup categories
 function wordsToMarkupCategories(words) {
-    if (words == "food") {
-        return ["food"];
+    // In reality this should be using a database and/or use some natural language library to
+    // group the word associations and deal with plurality (i.e. food vs foods)
+    
+    var out = []; //The output which will be a list of all applicable markup categories
+    
+    //Define the word associations    
+    var wordAssociations = {
+        "food": ["food", "foods", "cookie", "cookies"],
+        "pharmaceuticals": ["pharmaceutical", "pharmaceuticals", "drug", "drugs"],
+        "electronics": ["electronics", "electronic", "tv", "tvs"]
     }
-    if (words == "drugs") {
-        return ["pharmaceuticals"];
+
+    //Loop through the word association lists
+    for (var markupCategory in wordAssociations) {
+        if (wordAssociations.hasOwnProperty(markupCategory)) {
+            var wordList = wordAssociations[markupCategory];
+            
+            //Loop through the words for each category
+            for (var word of wordList) {
+                //If the association word is located anywhere in the entire word phrase, add the markup key to the output
+                if (words.toLowerCase().indexOf(word) !== -1){ 
+                    out.push(markupCategory);
+                    break; //We only want to add it at most once so we jump to the next list
+                }
+            }
+        }
     }
-    return [];
+    
+    return out;
 }
 
 module.exports = {
--- NuPackPriceCalculatorTests.js
+++ NuPackPriceCalculatorTests.js
@@ -1,217 +1,207 @@
 var expect = require('expect');
 var NPPC = require('./NuPackPriceCalculator');
 
- //Tests
- //3 Provided Examples
- console.log("Testing Basic Example 1: ($1,299.99, 3 people, food)");
- expect(
-	NPPC.priceCalc("$1,299.99", "3 people", "food")
- ).toEqual("$1,591.58");
- 
- console.log("Testing Basic Example 2: ($5,432.00, 1 person, drugs)");
- expect(
-	NPPC.priceCalc("$5,432.00", "1 person", "drugs")
- ).toEqual("$6,199.81");
- 
- console.log("Testing Basic Example 3: ($12,456.95, 4 people, books)");
- expect(
-	NPPC.priceCalc("$12,456.95", "4 people", "books")
- ).toEqual("$13,707.63");
- 
- 
- //Leading 0s
- console.log("Testing Leading 0s in Price pt1: ($0012,456.95, 4 people, books)");
- expect(
-	NPPC.priceCalc("$0012,456.95", "4 people", "books")
- ).toEqual("$13,707.63");
- 
- console.log("Testing Leading 0s in Price pt2: ($0,012,456.95, 4 people, books)");
- expect(
-	NPPC.priceCalc("$0,012,456.95", "4 people", "books")
- ).toEqual("$13,707.63");
- 
- console.log("Testing Leading 0s in Price pt3: (0012456.95, 4 people, books)");
- expect(
-	NPPC.priceCalc("0012456.95", "4 people", "books")
- ).toEqual("$13,707.63");
- 
- 
- //Trailing 0s
- console.log("Testing Trailing 0s in Price: ($1,299.99, 3 people, food)");
- expect(
-	NPPC.priceCalc("$1,299.9900000000000000000", "3 people", "food")
- ).toEqual("$1,591.58");
- 
- 
- //No decimal
- console.log("Testing No Decimals in Price Pt1: ($1,299.00, 3 people, food)");
- expect(
-	NPPC.priceCalc("$1,299.00", "3 people", "food")
- ).toEqual("$1,590.37");
- 
- console.log("Testing No Decimals in Price Pt2: ($1,299, 3 people, food)");
- expect(
-	NPPC.priceCalc("$1,299", "3 people", "food")
- ).toEqual("$1,590.37");
- 
- 
- //Long decimal (What's appropriate behaviour? Maybe attempt to round base price first?)
- console.log("Testing Long Decimals in Price: ($1,299.98756413135..., 3 people, food)");
- expect(
-	NPPC.priceCalc("$1,299.98756413135123152165325186513518312135351813212315132186541231653241946465121312356546", "3 people", "food")
- ).toEqual("$1,591.58");
- 
- 
- //Output has no decimals
- console.log("Testing Output Has No Decimals: ($1,301.97, 3 people, food)");
- expect(
-	NPPC.priceCalc("$1301.97", "3 people", "food")
- ).toEqual("$1594.00");
- 
- 
- //Each Category -- this was mostly tested with basic examples
- console.log("Testing Category pharmaceuticals exact: ($5,432.00, 1 person, pharmaceuticals)");
- expect(
-	NPPC.priceCalc("$5,432.00", "1 person", "pharmaceuticals")
- ).toEqual("$6,199.81");
- 
- 
- //$0
- console.log("Testing $0 base: ($0, 1 person, pharmaceuticals)");
- expect(
-	NPPC.priceCalc("$0", "1 person", "pharmaceuticals")
- ).toEqual("$0.00");
- 
- //Small base price
- console.log("Testing $1 base: ($1.00, 1 person, pharmaceuticals)");
- expect(
-	NPPC.priceCalc("$1.00", "3 people", "food")
- ).toEqual("$1.22");
- 
- //Large base price
- console.log("Testing Large Base: ($1,000,000,000.00, 3 people, food)");
- expect(
-	NPPC.priceCalc("$1,000,000,000.00", "3 people", "food")
- ).toEqual("$1,224,300,000");
- 
- 
- //1 person - tested by basic example
- 
- 
- //0 ppl
- console.log("Testing 0 People: ($1,299.99, 0 people, food)");
- expect(
-	NPPC.priceCalc("$1,299.99", "0 people", "food")
- ).toEqual("$1,542.44");
- 
- console.log("Testing 0 People and Other Category: ($1,299.99, 0 people, books)");
- expect(
-	NPPC.priceCalc("$1,299.99", "0 people", "books")
- ).toEqual("$1,364.99");
- 
- //Large amount of ppl
- console.log("Testing Large Amount of People: ($1,299.99, 1000 people, food)");
- expect(
-	NPPC.priceCalc("$1,299.99", "1000 people", "food")
- ).toEqual("$17,922.31");
- 
- //Large amount of ppl and big base
- console.log("Testing Large Amount of People w/ Large Base: ($1,299,000,000.99, 1000 people, food)");
- expect(
-	NPPC.priceCalc("$1,299,000,000.99", "1000 people", "food")
- ).toEqual("$17,908,663,513.65");
- 
- //0 ppl and big base
- console.log("Testing 0 People w/ Large Base: ($1,299,000,000.99, 0 people, food)");
- expect(
-	NPPC.priceCalc("$1,299,000,000.99", "0 people", "food")
- ).toEqual("$1,541,263,501.17");
- 
- 
- 
- 
- 
- //Parameter Problem Testing
- console.log("Testing Missing 1 Argument: ($1,299,000,000.99, food)");
- expect(
-	NPPC.priceCalc("$1,299.99",  "food")
- ).toThrow(/Argument Missing/);
- 
- console.log("Testing Missing 2 Arguments: (food)");
- expect(
-	NPPC.priceCalc("food")
- ).toThrow(/Argument Missing/);
- 
+//Tests
+console.log("Running Tests...");
+//3 Provided Examples
+console.log("Testing Basic Example 1: ($1,299.99, 3 people, food)");
+expect(
+    NPPC.priceCalc("$1,299.99", "3 people", "food")
+).toEqual("$1,591.58");
+
+console.log("Testing Basic Example 2: ($5,432.00, 1 person, drugs)");
+expect(
+    NPPC.priceCalc("$5,432.00", "1 person", "drugs")
+).toEqual("$6,199.81");
+
+console.log("Testing Basic Example 3: ($12,456.95, 4 people, books)");
+expect(
+    NPPC.priceCalc("$12,456.95", "4 people", "books")
+).toEqual("$13,707.63");
+
+
+//Leading 0s
+console.log("Testing Leading 0s in Price pt1: ($0012,456.95, 4 people, books)");
+expect(
+    NPPC.priceCalc("$0012,456.95", "4 people", "books")
+).toEqual("$13,707.63");
+
+console.log("Testing Leading 0s in Price pt2: ($0,012,456.95, 4 people, books)");
+expect(
+    NPPC.priceCalc("$0,012,456.95", "4 people", "books")
+).toEqual("$13,707.63");
+
+console.log("Testing Leading 0s in Price pt3: (0012456.95, 4 people, books)");
+expect(
+    NPPC.priceCalc("0012456.95", "4 people", "books")
+).toEqual("$13,707.63");
+
+
+//Trailing 0s
+console.log("Testing Trailing 0s in Price: ($1,299.99, 3 people, food)");
+expect(
+    NPPC.priceCalc("$1,299.9900000000000000000", "3 people", "food")
+).toEqual("$1,591.58");
+
+
+//No decimal
+console.log("Testing No Decimals in Price Pt1: ($1,299.00, 3 people, food)");
+expect(
+    NPPC.priceCalc("$1,299.00", "3 people", "food")
+).toEqual("$1,590.37");
+
+console.log("Testing No Decimals in Price Pt2: ($1,299, 3 people, food)");
+expect(
+    NPPC.priceCalc("$1,299", "3 people", "food")
+).toEqual("$1,590.37");
+
+
+//Long decimal (What's appropriate behaviour? Maybe attempt to round base price first?)
+console.log("Testing Long Decimals in Price: ($1,299.98756413135..., 3 people, food)");
+expect(
+    NPPC.priceCalc("$1,299.98756413135123152165325186513518312135351813212315132186541231653241946465121312356546", "3 people", "food")
+).toEqual("$1,591.58");
+
+
+//Output has no decimals
+console.log("Testing Output Has No Decimals: ($1,301.97, 3 people, food)");
+expect(
+    NPPC.priceCalc("$1301.97", "3 people", "food")
+).toEqual("$1,594.00");
+
+
+//Each Category -- this was mostly tested with basic examples
+console.log("Testing Category pharmaceuticals exact: ($5,432.00, 1 person, pharmaceuticals)");
+expect(
+    NPPC.priceCalc("$5,432.00", "1 person", "pharmaceuticals")
+).toEqual("$6,199.81");
+
+
+//$0
+console.log("Testing $0 base: ($0, 1 person, pharmaceuticals)");
+expect(
+    NPPC.priceCalc("$0", "1 person", "pharmaceuticals")
+).toEqual("$0.00");
+
+//Small base price
+console.log("Testing $1 base: ($1.00, 1 person, pharmaceuticals)");
+expect(
+    NPPC.priceCalc("$1.00", "3 people", "food")
+).toEqual("$1.22");
+
+//Large base price
+console.log("Testing Large Base: ($1,000,000,000.00, 3 people, food)");
+expect(
+    NPPC.priceCalc("$1,000,000,000.00", "3 people", "food")
+).toEqual("$1,224,300,000.00");
+
+
+//1 person - tested by basic example
+
+
+//0 ppl
+console.log("Testing 0 People: ($1,299.99, 0 people, food)");
+expect(
+    NPPC.priceCalc("$1,299.99", "0 people", "food")
+).toEqual("$1,542.44");
+
+console.log("Testing 0 People and Other Category: ($1,299.99, 0 people, books)");
+expect(
+    NPPC.priceCalc("$1,299.99", "0 people", "books")
+).toEqual("$1,364.99");
+
+//Large amount of ppl
+console.log("Testing Large Amount of People: ($1,299.99, 1000 people, food)");
+expect(
+    NPPC.priceCalc("$1,299.99", "1000 people", "food")
+).toEqual("$17,922.31");
+
+//Large amount of ppl and big base
+console.log("Testing Large Amount of People w/ Large Base: ($1,299,000,000.99, 1000 people, food)");
+expect(
+    NPPC.priceCalc("$1,299,000,000.99", "1000 people", "food")
+).toEqual("$17,908,663,513.65");
+
+//0 ppl and big base
+console.log("Testing 0 People w/ Large Base: ($1,299,000,000.99, 0 people, food)");
+expect(
+    NPPC.priceCalc("$1,299,000,000.99", "0 people", "food")
+).toEqual("$1,541,263,501.17");
+
+
+
+
+
+//Parameter Problem Testing
+console.log("Testing Missing 1 Argument: ($1,299.99, food)");
+expect(function () {
+    NPPC.priceCalc("$1,299.99", "food")
+}).toThrow(/Argument Missing/);
+
+console.log("Testing Missing 2 Arguments: (food)");
+expect(function () {
+    NPPC.priceCalc("food")
+}).toThrow(/Argument Missing/);
+
 
 //Badly Formatted Args
- console.log("Testing Badly Formatted Price: ($1,2.99.99, 3 people, food)");
- expect(
-	NPPC.priceCalc("$1,29.9.99", "3 people", "food")
- ).toThrow(/Price Badly Formatted/);
- 
- console.log("Testing Badly Formatted Price pt1: ($1,299.99, people 3 person 4, food)");
- expect(
-	NPPC.priceCalc("$1,29.9.99", "people 3 person 4", "food")
- ).toThrow(/People Badly Formatted/);
- 
- console.log("Testing Badly Formatted Price pt2: ($1,299.99, 3.5 people, food)");
- expect(
-	NPPC.priceCalc("$1,29.9.99", "3.5 people", "food")
- ).toThrow(/People Badly Formatted/);
- 
- console.log("Testing Badly Formatted Category: ($1,299.99, 3 people, 2)");
- expect(
-	NPPC.priceCalc("$1,29.9.99", "3 people", 2)
- ).toThrow(/Category Badly Formatted/);
- 
- console.log("Testing Negative Price: ($-1,299.99, 3 people, food)");
- expect(
-	NPPC.priceCalc("$-1,299.99", "3 people", "food")
- ).toThrow(/Price Badly Formatted/);
- 
- console.log("Testing Negative Price: ($1,299.99, -3 people, food)");
- expect(
-	NPPC.priceCalc("$1,299.99", "-3 people", "food")
- ).toThrow(/People Badly Formatted/);
- 
- //Arguments out of order
- console.log("Testing Negative Price: (food, $1,299.99, -3 people)");
- expect(
-	NPPC.priceCalc("food", "$1,299.99", "-3 people")
- ).toThrow(/Price Badly Formatted/);
- 
- //Arguments out of order
- console.log("Testing Category Not In Lower Case: ($1,299.99, 3 people, FoOd)");
- expect(
-	NPPC.priceCalc("$1,299.99", "3 people", "FoOd")
- ).toEqual("$1,591.58");
- 
- 
-//Markup Complexity Testing
- console.log("Testing Multiple Categories: ($1,299.99, 3 people, food and drugs)");
- expect(
-	NPPC.priceCalc("$1,299.99", "3 people", "food and drugs")
- ).toEqual("$1,693.95");
- 
- console.log("Testing Food Category Association pt1: ($1,299.99, 3 people, cookie)");
- expect(
-	NPPC.priceCalc("$1,299.99", "3 people", "cookie")
- ).toEqual("$1,591.58");
- 
- console.log("Testing Food Category Association pt2: ($1,299.99, 3 people, cookies)");
- expect(
-	NPPC.priceCalc("$1,299.99", "3 people", "cookies")
- ).toEqual("$1,591.58");
- 
- console.log("Testing Food Category Association pt3: ($1,299.99, 3 people, foods)");
- expect(
-	NPPC.priceCalc("$1,299.99", "3 people", "foods")
- ).toEqual("$1,591.58");
- 
- console.log("Testing Electronics Category Association pt3: ($1,299.99, 3 people, tv)");
- expect(
-	NPPC.priceCalc("$1,299.99", "3 people", "tv")
- ).toEqual("$1,441.43");
- 
+console.log("Testing Badly Formatted Price: ($1,2.99.99, 3 people, food)");
+expect(function () {
+    NPPC.priceCalc("$1,29.9.99", "3 people", "food")
+}).toThrow(/Price Badly Formatted/);
 
-console.log('Tests Passed!');
+console.log("Testing Badly Formatted People: ($1,299.99, 3.5 people, food)");
+expect(function () {
+    NPPC.priceCalc("$1,299.99", "3.5 people", "food")
+}).toThrow(/People Badly Formatted/);
+
+console.log("Testing Badly Formatted Category: ($1,299.99, 3 people, 2)");
+expect(function () {
+    NPPC.priceCalc("$1,299.99", "3 people", 2)
+}).toThrow(/Category Badly Formatted/);
+
+console.log("Testing Negative Price: ($-1,299.99, 3 people, food)");
+expect(function () {
+    NPPC.priceCalc("$-1,299.99", "3 people", "food")
+}).toThrow(/Price Badly Formatted/);
+
+console.log("Testing Negative People: ($1,299.99, -3 people, food)");
+expect(function () {
+    NPPC.priceCalc("$1,299.99", "-3 people", "food")
+}).toThrow(/People Badly Formatted/);
+
+//Category not all in lower case
+console.log("Testing Category Not In Lower Case: ($1,299.99, 3 people, FoOd)");
+expect(
+    NPPC.priceCalc("$1,299.99", "3 people", "FoOd")
+).toEqual("$1,591.58");
+
+
+//Markup Complexity Testing
+console.log("Testing Multiple Categories: ($1,299.99, 3 people, food and drugs)");
+expect(
+    NPPC.priceCalc("$1,299.99", "3 people", "food and drugs")
+).toEqual("$1,693.95");
+
+console.log("Testing Food Category Association pt1: ($1,299.99, 3 people, cookie)");
+expect(
+    NPPC.priceCalc("$1,299.99", "3 people", "cookie")
+).toEqual("$1,591.58");
+
+console.log("Testing Food Category Association pt2: ($1,299.99, 3 people, cookies)");
+expect(
+    NPPC.priceCalc("$1,299.99", "3 people", "cookies")
+).toEqual("$1,591.58");
+
+console.log("Testing Food Category Association pt3: ($1,299.99, 3 people, foods)");
+expect(
+    NPPC.priceCalc("$1,299.99", "3 people", "foods")
+).toEqual("$1,591.58");
+
+console.log("Testing Electronics Category Association pt3: ($1,299.99, 3 people, tv)");
+expect(
+    NPPC.priceCalc("$1,299.99", "3 people", "tv")
+).toEqual("$1,441.43");
+
+
+console.log('...All Tests Passed !!');
